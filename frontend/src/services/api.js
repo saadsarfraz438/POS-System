44aac07614f4e28ef77ac36a2a5d72ea7d79f6a6
saadsarfraz <saadsarfraz438@gmail.com>
2026-07-24 20:32:21 +0500

    Update the folders into Frontend and Backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5217/api',
+  baseURL: 'http://localhost:5298/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
