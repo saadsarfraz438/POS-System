61fda124e11a6f9e3cba58b754b98b3e1cd0a122
saadsarfraz <saadsarfraz438@gmail.com>
2026-07-29 12:48:50 +0500

    update the CSS layout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// @ts-expect-error CSS import is resolved by the bundler.
-import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
